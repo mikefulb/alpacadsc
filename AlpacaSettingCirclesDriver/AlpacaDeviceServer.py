a90a9cd27a320ad3cddc732ed0e4a2ac9ae770ff
Michael Fulbright <mike.fulbright@pobox.com>
2020-09-06 21:34:21 -0400

    Convert to use Flask

--- AlpacaSettingCirclesDriver/AlpacaDeviceServer.py
+++ AlpacaSettingCirclesDriver/AlpacaDeviceServer.py
@@ -2,8 +2,10 @@ import json
 import logging
 from threading import Thread
 
-from bottle import Bottle
-from bottle import request, response
+#from bottle import Bottle
+#from bottle import request, response
+
+from flask import Flask, request
 
 _alpaca_url_base = '/api/v1/telescope/0'
 
@@ -14,11 +16,19 @@ class AlpacaDeviceServer(Thread):
         self.device = device
         self.host = host
         self.port = port
-        self.app = Bottle()
-        self.app.route(_alpaca_url_base + '/<action>', method='GET',
-                       callback=self.core_get_action_handler)
-        self.app.route(_alpaca_url_base + '/<action>', method='PUT',
-                       callback=self.core_put_action_handler)
+        #self.app = Bottle()
+        self.app = Flask(__name__)
+
+        #self.app.route(_alpaca_url_base + '/<action>', method='GET',
+        #               callback=self.core_get_action_handler)
+        #self.app.route(_alpaca_url_base + '/<action>', method='PUT',
+        #               callback=self.core_put_action_handler)
+        #self.server_transaction_id = 0
+
+        self.app.add_url_rule(_alpaca_url_base + '/<action>', methods=['GET'],
+                              view_func=self.core_get_action_handler)
+        self.app.add_url_rule(_alpaca_url_base + '/<action>', methods=['PUT'],
+                              view_func=self.core_put_action_handler)
         self.server_transaction_id = 0
 
         # die if main dies
@@ -48,8 +58,8 @@ class AlpacaDeviceServer(Thread):
         resp['ErrorString'] = action_resp['ErrorString']
         resp['Value'] = action_resp['Value']
 
-        response.headers['Content-Type'] = 'application/json'
-        return json.dumps(resp)
+        #response.headers['Content-Type'] = 'application/json'
+        return json.dumps(resp), 200, {'Content-Type' : 'application/json'}
 
     def core_put_action_handler(self, action):
         #logging.debug(f'core_put_action_handler(): action = {action}')
@@ -61,7 +71,7 @@ class AlpacaDeviceServer(Thread):
 
         resp = {}
         try:
-            resp['ClientTransactionID'] = request.forms['ClientTransactionID']
+            resp['ClientTransactionID'] = request.form['ClientTransactionID']
         except:
             logging.warning('request missing client id!')
 
@@ -70,14 +80,14 @@ class AlpacaDeviceServer(Thread):
         resp['ErrorNumber'] = 0
         resp['ErrorString'] = ''
 
-        action_resp = self.device.put_action_handler(action, request.forms)
+        action_resp = self.device.put_action_handler(action, request.form)
 
         resp['ErrorNumber'] = action_resp['ErrorNumber']
         resp['ErrorString'] = action_resp['ErrorString']
 
-        response.headers['Content-Type'] = 'application/json'
-        return json.dumps(resp)
+        #response.headers['Content-Type'] = 'application/json'
+        return json.dumps(resp), 200, {'Content-Type' : 'application/json'}
 
     def run(self):
-        self.app.run(host=self.host, port=self.port, debug=True, quiet=True)
+        self.app.run(host=self.host, port=self.port) #, debug=True, quiet=True)
 
