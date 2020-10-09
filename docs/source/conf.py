dbb8471402bc4be775b50104840ca93ad04887a9
Michael Fulbright <mike.fulbright@pobox.com>
2020-10-08 23:32:59 -0400

    Set import path so consts.py is found

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 #sys.path.insert(0, os.path.abspath('../../AlpacaDSCDriver'))
-#sys.path.insert(0, os.path.abspath('../../tests'))
+sys.path.insert(0, os.path.abspath('../../tests'))
 sys.path.insert(0, os.path.abspath('../../'))
 
 
